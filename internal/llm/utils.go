9bc3a649e8bec555640a3b240301a1af2100c996
AeronCyther <ayushsanjivnaidu@gmail.com>
2024-08-07 12:25:49 +0530

    refactor: make system prompt a constant for better readability

--- internal/llm/utils.go
+++ internal/llm/utils.go
@@ -7,11 +7,21 @@ import (
 	"github.com/AeronCyther/leet_tutor/internal/problem"
 )
 
+const EXPLAIN_PROBLEM_SYSTEM_PROMPT = `You are an expert problem solver capable of solving the toughest of coding problems.
+Given a problem, explain in detail the problem statement to the user.
+Give examples to go along with your explanation.
+Do not refuse to explain or say that no further explanation is needed.
+Your response should not contain anything other than the explanation.
+Use markdown syntax.
+Do not wrap your response with any symbol.
+Do not include a title or any text preceding the explanation.
+Do not repeat the problem statement or the constraints.`
+
 func ExplainProblemContent(p *problem.Problem) (string, error) {
 	response, err := llmAgent.Chat([]*Message{
 		{
 			Role:    "system",
-			Content: "You are an expert problem solver capable of solving the toughest of coding problems. Given a problem, explain in detail the problem statement to the user. Give examples to go along with your explanation. Do not refuse to explain or say that no further explanation is needed. Your response should not contain anything other than the explanation. Use markdown syntax. Do not wrap your response with any symbol. Do not include a title or any text preceding the explanation. Do not repeat the problem statement or the constraints.",
+			Content: EXPLAIN_PROBLEM_SYSTEM_PROMPT,
 		},
 		{
 			Role: "user",
